21de6d1558d181b8c2e6499c04cdb76631e745a1
Anasurim <108210477+Anasurim@users.noreply.github.com>
2023-05-26 21:45:28 +0000

    Deploying to gh-pages from @ Anasurim/goit-react-hw-05-movies@84c46dc834a86c064bb1463ea06a008853230810 🚀

---
+++ static/js/740.db2eff0f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[740],{740:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h2",{children:"Movie Search"})}}}]);
+//# sourceMappingURL=740.db2eff0f.chunk.js.map
